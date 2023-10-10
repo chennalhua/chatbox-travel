21b5cad7c2b5e9ca2dc2e6be66a13b30bccc387c
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2023-10-10 15:27:44 +0800

    [Update -- Initialize environment settings]

---
+++ src/components/WeatherBox.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+const WeatherBox = () => {
+    return (
+        <>
+        </>
+    )
+}
+export default WeatherBox
---
+++ src/view/HomePage.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+const HomePage = () => {
+    return (
+        <>
+        </>
+    )
+}
+export default HomePage
