8f0fd0ce0c75d6b66fa594b0c3b6cbada300d2a0
naihua.chen <vip899629@gmail.com>
2023-12-14 21:16:03 +0800

    [Update --  home page 設定]

--- src/view/HomePage.jsx
+++ src/view/HomePage.jsx
@@ -1,64 +1,88 @@
 import React, { useEffect, useState } from "react";
-import Nav from "components/layout/Nav";
-import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
-import Icon from "components/Icon";
-import dayjs from "dayjs";
-import { getWeek } from "assets/function/dateTool";
 import { callWeather } from "API/callWeather";
 import WeatherBox from "components/WeatherBox";
 import callPositionInfo from "API/callPostionInfo";
-import Map from "components/Map";
+import { Decrypt } from "assets/function/AES";
 const HomePage = () => {
   //@ VALUE
   let [weatherData, setWeatherData] = useState(null),
-    [positionInfoData, setPositionInfoData] = useState(null)
+    [positionInfoData, setPositionInfoData] = useState(null);
 
   //@ EVENT
   useEffect(() => {
     const getWeatherAPI = async (val) => {
       const response = await callWeather(val);
-      response[0] && setWeatherData(response[1])
-    }
+      response[0] && setWeatherData(response[1]);
+    };
 
     const getPositionInfoAPI = async () => {
-      const res = await callPositionInfo()
-      let { ResponseCode, ResponseData } = res
+      const res = await callPositionInfo();
+      let { ResponseCode, ResponseData } = res;
       switch (ResponseCode) {
-        case 'success': return (setPositionInfoData(ResponseData), getWeatherAPI(ResponseData.city))
-        case 'error': return //!顯示抓不到
-        default: break;
+        case "success":
+          return (
+            setPositionInfoData(ResponseData), getWeatherAPI(ResponseData.city)
+          );
+        case "error":
+          return; //!顯示抓不到
+        default:
+          break;
       }
-    }
-    getPositionInfoAPI()
+    };
+    getPositionInfoAPI();
+  }, []);
 
-  }, [])
+  let [userName, setUserName] = useState(null);
+  let checkLogin = localStorage.getItem("_LOGINDATA");
+  useEffect(() => {
+    if (checkLogin !== null) {
+      setUserName(JSON.parse(Decrypt(checkLogin))?.name);
+    }
+  }, [checkLogin]);
 
   return (
     <div
       className="bg-primary-light pb-4"
       style={{ height: "85vh", overflow: "scroll" }}
     >
-      {positionInfoData !== null &&
+      {positionInfoData !== null && (
         <>
-          <Map position={[positionInfoData?.latitude, positionInfoData?.longitude]} />
           <div className="container">
             <div className="row">
-              <div className="col-12 col-md-6">
-                <div className="mt-4 ms-2">
-                  <div className="rounded p-3" style={{ background: `rgba(255,255,255,0.3)` }}>
-                    您目前位置是
-                    <p>{positionInfoData.city}</p>
-                    <p>{positionInfoData.locality}</p>
+              <div className="col-12">
+                <div className="row mt-4 align-items-center">
+                  <div className="col-sm-4 text-center">
+                    <img
+                      src={require("assets/image/robot_.png")}
+                      className="img-fluid"
+                      width="150px"
+                    />
+                  </div>
+                  <div className="col-sm-8">
+                    <div className="message-box">
+                      <div className="text">
+                        <span
+                          className="fw-bolder"
+                          style={{ fontSize: "18px" }}
+                        >
+                          <span style={{ fontSize: "26px" }}>👋</span>,{" "}
+                          {userName}
+                        </span>
+                        <span className="mt-2 d-block">
+                          歡迎使用！您準備踏上一段冒險之旅尋找放鬆身心的景點。我將提供您景點提示、美食推薦。一起打造出美好回憶的旅程吧！
+                        </span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="col-12 col-md-6">
+              <div className="col-12">
                 {weatherData !== null && <WeatherBox data={weatherData} />}
               </div>
             </div>
           </div>
         </>
-      }
+      )}
     </div>
   );
 };
