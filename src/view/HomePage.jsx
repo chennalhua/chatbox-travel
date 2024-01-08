a23652abef75bdca42d869d5fbac37dbde6ba29f
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2024-01-08 09:04:53 +0800

    [Update -- getPositionInfoAPI ]

--- src/view/HomePage.jsx
+++ src/view/HomePage.jsx
@@ -6,7 +6,7 @@ import { Decrypt } from "assets/function/AES";
 const HomePage = () => {
   //@ VALUE
   let [weatherData, setWeatherData] = useState(null),
-    [positionInfoData, setPositionInfoData] = useState("嘉義縣");
+    [positionInfoData, setPositionInfoData] = useState(null);
 
   //@ EVENT
   useEffect(() => {
@@ -21,13 +21,11 @@ const HomePage = () => {
       switch (ResponseCode) {
         case "success":
           return (
-            setPositionInfoData({ city: "嘉義縣" }),
-            getWeatherAPI(ResponseData.city)
+            setPositionInfoData(ResponseData), getWeatherAPI(ResponseData.city)
           );
         case "error":
-          return getWeatherAPI("嘉義"); //!顯示抓不到
-        default:
-          return getWeatherAPI("嘉義");
+          return getWeatherAPI('嘉義') //!顯示抓不到
+        default: return getWeatherAPI('嘉義')
       }
     };
     getPositionInfoAPI();
